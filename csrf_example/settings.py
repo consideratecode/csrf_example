61f22355b56d0e5015d8a37c876333907e1c719f
Daniel Hepper <daniel.hepper@epicco.de>
2017-08-31 21:23:27 +0200

    Configured logging

--- csrf_example/settings.py
+++ csrf_example/settings.py
@@ -133,5 +133,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
 LOGIN_URL = 'chirp:create_user'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
